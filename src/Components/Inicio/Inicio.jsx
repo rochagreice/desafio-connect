7327057112bc1b5697fa55236aba065dd3f6746f
rochagreice <greicelr@gmail.com>
2026-02-04 19:33:54 -0300

    finalizamos a navBar e iniciamos a tela inicial

--- src/Components/Inicio/Inicio.jsx
+++ src/Components/Inicio/Inicio.jsx
@@ -1,9 +1,17 @@
+import styles from "./Inicio.module.scss";
+import ImgInicial from "../../assets/img-tela-inicial.png"
 function Inicio() {
   return (
-    <>
-    
-    <h2> Eu sou Inicio</h2>
-    </>
-  )
+    <section className={styles.containerTelaInicial}>
+      <div>
+        <h1> Projetos Sociais que transformam </h1>
+        <p>
+          Conectamos sua empresa com projetos sociais impactantes. Juntos,
+          criamos mudanças reais na comunidade.
+        </p>
+      </div>
+      <img src={ImgInicial} alt="Imagem da tela inicial" />
+    </section>
+  );
 }
-export default Inicio
+export default Inicio;
